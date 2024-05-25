fb2e62864936b18b1f61a45fb9e25fc1eec6cb84
konkeydong <dittoes.1@gmail.com>
2024-05-24 22:24:44 -0500

    add mp watcher

--- FFVI_T_Edition/FF6_holy_dragon_hp.lua
+++ FFVI_T_Edition/FF6_holy_dragon_hp.lua
@@ -1,15 +1,18 @@
-local addr        = 0x003BFC
+local HPAddress   = 0x003BFC
+local MPAddress = 0x003C10
 local statusByte1 = 0x003E7C
 local statusByte2 = 0x00327D
 
 local regenBit  = 2   -- 0b00000010
 local slowBit   = 4   -- 0b00000100
+local hasteBit  = 8   -- 0b00001000
 local shellBit  = 32  -- 0b00100000
 local safeBit   = 64  -- 0b01000000
 local floatBit  = 128 -- 0b10000000
 
 local regenFlag = 0
 local slowFlag  = 0
+local hasteFlag = 0
 local shellFlag = 0
 local safeFlag  = 0
 local floatFlag = 0
@@ -22,11 +25,13 @@ function boolToString(flag)
 end
 
 while true do
-	monsterHP = memory.read_u16_le(addr)
+	monsterHP = memory.read_u16_le(HPAddress)
+	monsterMP = memory.read_u16_le(MPAddress)
 	
 	statByte  = memory.read_s8(statusByte1)
 	regenFlag = bit.band(statByte, regenBit)
 	slowFlag  = bit.band(statByte, slowBit)
+	hasteFlag = bit.band(statByte, hasteBit)
 	shellFlag = bit.band(statByte, shellBit)
 	safeFlag  = bit.band(statByte, safeBit)
 	
@@ -34,11 +39,13 @@ while true do
 	floatFlag = bit.band(statByte, floatBit)
 	
 	gui.text(25, 25,  "HP Remaining: " .. monsterHP)
-	gui.text(25, 50,  "Regen Flag  : " .. boolToString(regenFlag))
-	gui.text(25, 75,  "Slow Flag   : " .. boolToString(slowFlag))
-	gui.text(25, 100, "Shell Flag  : " .. boolToString(shellFlag))
-	gui.text(25, 125, "Safe Flag   : " .. boolToString(safeFlag))
-	gui.text(25, 150, "Float Flag  : " .. boolToString(floatFlag))
+	gui.text(25, 50,  "MP Remaining: " .. monsterMP)
+	gui.text(25, 75,  "Regen Flag  : " .. boolToString(regenFlag))
+	gui.text(25, 100, "Slow Flag   : " .. boolToString(slowFlag))
+	gui.text(25, 125, "Haste Flag  : " .. boolToString(hasteFlag))
+	gui.text(25, 150, "Shell Flag  : " .. boolToString(shellFlag))
+	gui.text(25, 175, "Safe Flag   : " .. boolToString(safeFlag))
+	gui.text(25, 200, "Float Flag  : " .. boolToString(floatFlag))
 		
 	emu.frameadvance();
 end
