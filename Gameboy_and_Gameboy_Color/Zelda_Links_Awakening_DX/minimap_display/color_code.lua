98e13483098aec55b8c63d3f1f299c87ab1e256a
konkeydong <dittoes.1@gmail.com>
2024-08-21 00:33:46 -0500

    refactor file structure

---
+++ Gameboy_and_Gameboy_Color/Zelda_Links_Awakening_DX/minimap_display/color_code.lua
@@ -0,0 +1,13 @@
+return {
+  empty = "black",
+  room  = "white",
+  dungeon_entrance = "green",
+  instrument = "magenta",
+  link = "yellow",
+  boss = "red",
+  dungeon = "brown",
+  water = "cyan",
+  forest = "lightgreen",
+  desert = "orange",
+  graveyard = "lightgray"
+}
